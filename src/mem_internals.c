a497a6c42913735d284072186c471dcac0f4e938
Juan Jose Garcia Duarte <gdjuanjojr@gmail.com>
2024-10-31 00:57:20 +0100

    feat: WIP Mark memory blocks

--- src/mem_internals.c
+++ src/mem_internals.c
@@ -7,59 +7,80 @@
 #include <sys/mman.h>
 #include <assert.h>
 #include <stdint.h>
-#include "mem.h"
 #include "mem_internals.h"
+#include <bits/mman-linux.h>
 
 unsigned long knuth_mmix_one_round(unsigned long in)
 {
     return in * 6364136223846793005UL % 1442695040888963407UL;
 }
 
+// unsigned long calculate_magic_number(unsigned long )
+
 void *mark_memarea_and_get_user_ptr(void *ptr, unsigned long size, MemKind k)
 {
-    /* ecrire votre code ici */
-    return (void *)0;
+
+    // ->[SIZE][MAGIC NUM]-- [USER MEMORY]--[MAGIC NUM][SIZE]
+    //
+    char *size_mark_ptr;
+    void *detonacion;
+    unsigned long magic_number = knuth_mmix_one_round((unsigned long) ptr);
+    *(unsigned long *)ptr = size;
+    *((unsigned long *)ptr + 1) = magic_number;
+    *((char *)ptr + size - 1) = size;
+    *((char *)ptr + size - 2) = magic_number;
+
+    detonacion = (void *) 0x1000;
+    int *int_pointer = (int *)detonacion + 1;
+    long *long_pointer = (long *)detonacion + 1;
+    printf("int_pointer : %p\n", int_pointer);
+    printf("long_pointer : %p\n", long_pointer);
+
+    size_mark_ptr = ptr + size - 8;
+    printf("this is the initial size: %lu\n", size);
+    printf("this is the size in memory: %lu\n", *(unsigned long *)size_mark_ptr);
+
+    return (void *)((unsigned long *)ptr + 2);
 }
 
-Alloc
-mark_check_and_get_alloc(void *ptr)
+Alloc mark_check_and_get_alloc(void *ptr)
 {
     /* ecrire votre code ici */
     Alloc a = {};
     return a;
 }
 
-
 unsigned long
-mem_realloc_small() {
+mem_realloc_small()
+{
     assert(arena.chunkpool == 0);
     unsigned long size = (FIRST_ALLOC_SMALL << arena.small_next_exponant);
     arena.chunkpool = mmap(0,
-			   size,
-			   PROT_READ | PROT_WRITE | PROT_EXEC,
-			   MAP_PRIVATE | MAP_ANONYMOUS,
-			   -1,
-			   0);
+                           size,
+                           PROT_READ | PROT_WRITE | PROT_EXEC,
+                           MAP_PRIVATE | MAP_ANONYMOUS,
+                           -1,
+                           0);
     if (arena.chunkpool == MAP_FAILED)
-	handle_fatalError("small realloc");
+        handle_fatalError("small realloc");
     arena.small_next_exponant++;
     return size;
 }
 
 unsigned long
-mem_realloc_medium() {
+mem_realloc_medium()
+{
     uint32_t indice = FIRST_ALLOC_MEDIUM_EXPOSANT + arena.medium_next_exponant;
     assert(arena.TZL[indice] == 0);
     unsigned long size = (FIRST_ALLOC_MEDIUM << arena.medium_next_exponant);
-    assert( size == (1 << indice));
     arena.TZL[indice] = mmap(0,
-			     size*2, // twice the size to allign
-			     PROT_READ | PROT_WRITE | PROT_EXEC,
-			     MAP_PRIVATE | MAP_ANONYMOUS,
-			     -1,
-			     0);
+                             size * 2, // twice the size to allign
+                             PROT_READ | PROT_WRITE | PROT_EXEC,
+                             MAP_PRIVATE | MAP_ANONYMOUS,
+                             -1,
+                             0);
     if (arena.TZL[indice] == MAP_FAILED)
-	handle_fatalError("medium realloc");
+        handle_fatalError("medium realloc");
     // align allocation to a multiple of the size
     // for buddy algo
     arena.TZL[indice] += (size - (((intptr_t)arena.TZL[indice]) % size));
@@ -67,15 +88,15 @@ mem_realloc_medium() {
     return size; // lie on allocation size, but never free
 }
 
-
 // used for test in buddy algo
 unsigned int
-nb_TZL_entries() {
+nb_TZL_entries()
+{
     int nb = 0;
-    
-    for(int i=0; i < TZL_SIZE; i++)
-	if ( arena.TZL[i] )
-	    nb ++;
+
+    for (int i = 0; i < TZL_SIZE; i++)
+        if (arena.TZL[i])
+            nb++;
 
     return nb;
 }
