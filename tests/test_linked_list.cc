80a38eaee6b847a03c08fb1b18b26a623acab074
Juan José García Duarte <gdjuanjojr@gmail.com>
2025-03-23 22:48:15 +0100

    feat(medium): Linked list feature test
     Add  edge cases tests for remove_element

--- tests/test_linked_list.cc
+++ tests/test_linked_list.cc
@@ -54,7 +54,7 @@ TEST(Linked_list, poll)
   ASSERT_EQ(head, (void *)NULL);
 }
 
-TEST(Linked_list, remove_element)
+TEST(Linked_list, remove_element_at_the_middle)
 {
 
   // Arrange
@@ -78,4 +78,56 @@ TEST(Linked_list, remove_element)
   // Assert
   ASSERT_EQ(*(void **)second_element, fourth_element);
   ASSERT_EQ(removed_block, third_element);
+}
+
+TEST(Linked_list, remove_element_pointed_by_head)
+{
+
+  // Arrange
+  //  I need a block of mem
+  unsigned long size = 1 << 17;
+  unsigned long element_size = 1 << 15;
+  void *memory_block = mmap(0,
+                            size * 2, // twice the size to allign
+                            PROT_READ | PROT_WRITE | PROT_EXEC,
+                            MAP_PRIVATE | MAP_ANONYMOUS,
+                            -1,
+                            0);
+  void *head = new_linked_list((void **)memory_block, size, element_size);
+  void *second_element = (char *)memory_block + 1 * element_size;
+  void *third_element = (char *)memory_block + 2 * element_size;
+  void *old_head = head;
+
+  // Act
+  void *removed_block = remove_element(&head, head);
+
+  // Assert
+  ASSERT_EQ(old_head, removed_block);
+  ASSERT_EQ(head, second_element);
+  ASSERT_EQ(*(void **)head, third_element);
+}
+
+TEST(Linked_list, remove_element_at_the_end)
+{
+
+  // Arrange
+  //  I need a block of mem
+  unsigned long size = 1 << 17;
+  unsigned long element_size = 1 << 15;
+  void *memory_block = mmap(0,
+                            size * 2, // twice the size to allign
+                            PROT_READ | PROT_WRITE | PROT_EXEC,
+                            MAP_PRIVATE | MAP_ANONYMOUS,
+                            -1,
+                            0);
+  void *head = new_linked_list((void **)memory_block, size, element_size);
+  void *third_element = (char *)memory_block + 2 * element_size;
+  void *fourth_element = (char *)memory_block + 3 * element_size;
+
+  // Act
+  void *removed_block = remove_element(&head, fourth_element);
+
+  // Assert
+  ASSERT_EQ(*(void **)third_element, (void *)NULL);
+  ASSERT_EQ(removed_block, fourth_element);
 }
